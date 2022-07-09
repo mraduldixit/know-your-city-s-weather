9c21c7173a713af3852ed4ddafd3418c9f679dd4
shrimaliashish <ashishshrimali9935@gmail.com>
2022-07-09 16:46:29 +0530

    Add files via upload
    "v-2.0"

---
+++ build/precache-manifest.d6db95ec4703c5c125f5eeaaaf3ea63f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9998c41f7201658e3d2e4358b57b60a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "390d4977a3fb812d59b1",
+    "url": "/static/css/main.23d7dc75.chunk.css"
+  },
+  {
+    "revision": "320bdf175924e2c56136",
+    "url": "/static/js/2.d00f01d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d00f01d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "390d4977a3fb812d59b1",
+    "url": "/static/js/main.1f145eae.chunk.js"
+  },
+  {
+    "revision": "30259e7a30d4e8b4ab16",
+    "url": "/static/js/runtime-main.a9559881.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "75e0f8ca422777deab777d3abe0a90e7",
+    "url": "/static/media/background.75e0f8ca.jpg"
+  },
+  {
+    "revision": "e1698332435d6a6fb48294f85157aeab",
+    "url": "/static/media/city.e1698332.jpg"
+  }
+]);
