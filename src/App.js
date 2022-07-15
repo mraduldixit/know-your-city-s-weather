cf763fb98b4463c85201b8e80f0b95d8c9af18a3
Mradul Dixit <mraduldixit2002@gmail.com>
2022-07-15 12:07:00 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import "./App.css";
 function App() {
   return (
     <React.Fragment>
-      <div className="container">
-        <CurrentLocation />
-      </div>
-      <div className="footer-info">
-        Developed by{" "}
-        <a  href="https://github.com/mraduldixit/">
-         <span>Mradul Dixit</span> 
-        </a>
-        
+      <div className="containers">
+        <div className="container">
+          <CurrentLocation />
+        </div>
+        <div className="footer-info">
+          <a href="https://github.com/mraduldixit/">
+            <span>Mradul Dixit</span>
+          </a>
+        </div>
       </div>
     </React.Fragment>
   );
